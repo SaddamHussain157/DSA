d1efd39d639317cf2d6a519eac296aeb24b369c3
SaddamHussain157 <saddamhussainsumalani109@gmail.com>
2024-03-18 14:02:37 +0500

    Adding new class and methods

---
+++ Pattern.java
@@ -0,0 +1,7 @@
+package DSA;
+
+class Pattern {
+    Pattern(){
+        System.out.println("Hello saddam");
+    }
+}
---
+++ hello.java
@@ -0,0 +1,11 @@
+package DSA;
+/**
+ * hello
+ */
+import DSA.Pattern;
+public class hello {
+
+    public static void main(String[] args) {
+        Pattern obj = new Pattern();
+    }
+}
